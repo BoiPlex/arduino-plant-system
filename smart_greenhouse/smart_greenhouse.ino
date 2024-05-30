760cc66f1300a5fdf994dc45e214c4b4b5a78338
Karim <kkarimshami@gmail.com>
2024-05-29 17:39:22 -0700

    Code compiles

--- smart_greenhouse/smart_greenhouse.ino
+++ smart_greenhouse/smart_greenhouse.ino
@@ -1,27 +1,28 @@
-#include <Adafruit_LiquidCrystal.h>
-Adafruit_LiquidCrystal lcd_1(0);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd_1(0x20, 16, 2);
 
 // Inputs
 int buttonPin = 7; // switch between 2 modes: blueberry and spinach
-int lightPin = A0;
-int TDSPin = A1;
+int lightPin = A1;
+int TDSPin = A3;
 int moisturePin = A2;
 
 // Outputs
-int redPin = 10;
-int greenPin = 8;
-int bluePin = 9;
-int pumpPin = 11;
+int redPin = 13;
+int greenPin = 11;
+int bluePin = 12;
+int pumpPin = 2;
 
 // Blueberry and spinach modes
 int mode = 0; // 0 for blueberry (default), 1 for spinach
 int lightLimit[] = {100, 200};
-int TDSRange[][] = {{500, 1000}, {600, 1100}};
+int TDSRange[][2] = {{500, 1000}, {600, 1100}};
 int moistureLimit[] = {300, 400};
 
 void setup() {
-    lcd_1.begin(16, 2);
-    // lcd_1.print("hello world");
+    lcd_1.init();
+    lcd_1.setBacklight(1);
+    lcd_1.print("Starting...");
 
     pinMode(buttonPin, INPUT);
     pinMode(lightPin, INPUT);
@@ -32,9 +33,12 @@ void setup() {
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
     pinMode(pumpPin, OUTPUT);
+
+    delay(1000);
 }
 
 void loop() {
+    lcd_1.clear();
     // Toggle mode if button is pressed
     if (digitalRead(buttonPin) == HIGH) {
         if (mode == 0) mode = 1;
@@ -44,8 +48,11 @@ void loop() {
     float lightLevel = analogRead(lightPin);
     float TDSLevel = analogRead(TDSPin);
     float moistureLevel = analogRead(moisturePin);
-    // Not sure how much you can fit into the LED, you'll have to test it
-    // The LED's first row is reserved for the status message
+
+    // Not sure how may rows we can fit into the LCD, we'll have to test it
+
+    // The LCD's first row is reserved for the status message
+    // Other LCD output:
     lcd_1.setCursor(0, 1);
     lcd_1.print(lightLevel);
     lcd_1.setCursor(0, 2);
@@ -72,13 +79,13 @@ void loop() {
         }
         else { // Proper TDS
             if (moistureLevel < moistureLimit[mode]) { // Low moisture
-                analogWrite(pumpPin, 100) // activate pump
+                analogWrite(pumpPin, 100); // activate pump
                 setLED(0, 0, 255); // blue
                 lcd_1.setCursor(0, 0);
                 lcd_1.print("Pumping water!");
             }
             else { // Everything is good
-                analogWrite(pumpPin, 0) // deactivate pump
+                analogWrite(pumpPin, 0); // deactivate pump
                 setLED(0, 255, 0); // green
                 lcd_1.setCursor(0, 0);
                 lcd_1.print("Everything is good!");
@@ -90,17 +97,8 @@ void loop() {
     delay(500);
 }
 
-void setLED(red, green, blue) {
+void setLED(int red, int green, int blue) {
     analogWrite(redPin, red);
     analogWrite(redPin, green);
     analogWrite(redPin, blue);
 }
-
-/* For reference (delete later)
-
-lcd_1.setCursor(0, 1);
-lcd_1.print(seconds);
-
-lcd_1.setBacklight(0);
-delay(500);
-*/
